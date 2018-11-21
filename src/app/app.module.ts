4f89b63f2895e776c00a2f92e420a9c3bc650ee7
Elizabeth Allen <elizabethallen36@gmail.com>
2018-11-20 18:32:48 -0800

    day5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent
+    TodoComponent,
+    ContactComponent
   ],
   imports: [
     AppRoutingModule,
