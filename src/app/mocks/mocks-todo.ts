1e2a7189cfb7c32e8e3a257c226a025d2332f730
Elizabeth Allen <elizabethallen36@gmail.com>
2018-11-20 18:20:32 -0800

    day 5

--- src/app/mocks/mocks-todo.ts
+++ src/app/mocks/mocks-todo.ts
@@ -1,9 +1,9 @@
 import { Todo } from '../model/todo';
 
 export const TODO: Todo[] = [
-    { id: 1, name: 'Take out trash', tags: ['Weekly'], isCompleted: false, isDeleted: false },
+    { id: 1, name: 'Take out trash', tags: ['Daily','Weekly'], isCompleted: false, isDeleted: false },
     { id: 2, name: 'Walk the dog', tags: ['Daily'], isCompleted: false, isDeleted: false },
     { id: 3, name: 'Clean the bathroom', tags: ['Weekly'], isCompleted: false, isDeleted: false },
-    { id: 4, name: 'Do the dishes', tags: ['Daily'], isCompleted: false, isDeleted: false },
-    { id: 5, name: 'Mow the lawn', tags: ['Monthly'], isCompleted: false, isDeleted: false }
+    { id: 4, name: 'Do the dishes', tags: ['Daily', 'Weekly'], isCompleted: false, isDeleted: false },
+    { id: 5, name: 'Mow the lawn', tags: ['Monthly', 'Yearly'], isCompleted: false, isDeleted: false }
 ];
