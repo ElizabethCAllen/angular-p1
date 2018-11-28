4d23d0b50bbdd94f324c9f5bd8882d4eeb5fa9ef
Elizabeth Allen <elizabethallen36@gmail.com>
2018-11-27 19:37:32 -0800

    day7

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { HEROES} from '../mocks/ mocks-heroes';
+import { HeroInterface} from '../interface/hero-interface';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+
+
   constructor() { }
+    getHeroes(filter){
+      if (filter === 'all') {return HEROES;}
+      return HEROES.filter();
+    }
+  }
 }
