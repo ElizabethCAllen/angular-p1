1e1b1eaa455196b901f781f9fb1f493ad18dfaf3
ElizabethCAllen <36873561+ElizabethCAllen@users.noreply.github.com>
2018-12-11 20:22:13 -0800

    day 11

--- src/app/amiibos/amiibos.component.ts
+++ src/app/amiibos/amiibos.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AmiiboService, AmiiboService } from "../services/amiibo.service";
-import { AmiiboInterface } from "../interface/amiibo-interface";
-import { paginationService } from '../serives/pagination.service'
+import { AmiiboService, AmiiboService } from '../services/amiibo.service';
+import { AmiiboInterface } from '../interface/amiibo-interface';
 import { PaginationService } from '../services/pagination.service';
 
 @Component({
@@ -13,23 +12,23 @@ import { PaginationService } from '../services/pagination.service';
 export class AmiibosComponent implements OnInit {
 
 amiibos: AmiiboInterface[];
-  paginator:{};
-  pageItem; AmiibosInterface[] = [];
+  paginator: any = {};
+  pagedItem: AmiiboInterface[] = [];
 
 
   constructor( private amiibo: AmiiboService, private paginationSerive: PaginationService) { }
 
   getAmiibos() {
-    this.amiibos= this.amiibo.getAmiibos();
+    this.amiibos = this.amiibo.getAmiibos();
   }
-  setPage(page: number){
-    if(page <1 \\ this.paginator.totalPages) {return;}
-    const amiiboCount= this.amiibos.length;
-    this.paginator = this.paginationSerive.getPaginator( amiiboCount, page, itemsOnPage:12);
-    const startIndex= this.paginator.startIndex;
-    const endIndex = this.paginator.endIndex +1;
+  setPage(page: number) {
+    if (page < 1 || this.paginator.totalPages) { return; }
+    const amiiboCount = this.amiibos.length;
+    this.paginator = this.paginationSerive.getPaginator( amiiboCount, page, itemsOnPage: 12 );
+    const startIndex = this.paginator.startIndex;
+    const endIndex = this.paginator.endIndex + 1 ;
     this.pagedItems = this.amiibos.slice(this.paginator.startIndex, endIndex);
-    
+
   }
 
   ngOnInit() {
